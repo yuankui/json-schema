146e1e36d3e8dc18a066ccc08538eae2cc3b00b3
yuankui <yuankui@youzan.com>
2018-06-26 11:39:26 +0800

    up

---
+++ src/main/java/com/yuankui/jsonschema/JsonSchemaApplication.java
@@ -0,0 +1,12 @@
+package com.yuankui.jsonschema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonSchemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonSchemaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/yuankui/jsonschema/JsonSchemaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yuankui.jsonschema;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JsonSchemaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
